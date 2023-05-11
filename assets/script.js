cc2c918ee4a79625992548488f19785256d453f5
TheWiseSailor <68026214+TheWiseSailor@users.noreply.github.com>
2023-05-11 08:18:53 -0400

    v1.39 still cant figure out how to debugg this question, but the minimum question requirement from the instructor is 5 so I will leave it at 5

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,41 @@
+//AS A coding boot camp student
+//I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+//SO THAT I can gauge my progress compared to my peers
+//GIVEN I am taking a code quiz
+//WHEN I click the start button
+//THEN a timer starts and I am presented with a question
+//WHEN I answer a question
+//THEN I am presented with another question
+//WHEN I answer a question incorrectly
+//THEN time is subtracted from the clock
+//WHEN all questions are answered or the timer reaches 0
+//THEN the game is over
+//WHEN the game is over
+//THEN I can save my initials and score
+//made changes to js not html, my fault lol
+//adding a variable for the timer
+
 var time = document.querySelector(".timer");
 var score = document.querySelector("#score");
 var secondsLeft = 75;
-
+//adding a variable for the buttons
 var start = document.querySelector("#start");
-
+//making a variable for the intro startt button so that the challenge begins
 var quizIntro = document.querySelector("#challenge-begins"); //all changed to quizIntro
-
+//calling the end load element variable
 var questionsEl = document.querySelector(".all-question");
-
+//identifying the element locations and variables
 let questionEl = document.querySelector("#question");
 var correctWrong = document.querySelector("#right-wrong");
 let questionCount = 0;
-
+//making a final score variable
 var finalEl = document.querySelector("#final-score");
 let initialsInput = document.querySelector("#initials");
-
+//making a Highscore variable as well
 var highscoresEl = document.querySelector("#high-scores");
 let scoreListEl = document.querySelector(".score-list");
 let scoreList = [];
-
+//marking the answer class button
 var ansBtn = document.querySelectorAll("button.answer-btn");
 let submitScrBtn = document.querySelector("#submit-score");
 let clearScrBtn = document.querySelector("#clearScores");
@@ -28,7 +45,9 @@ var ans1Btn = document.querySelector("#answer-1");
 var ans2Btn = document.querySelector("#answer-2");
 var ans3Btn = document.querySelector("#answer-3");
 var ans4Btn = document.querySelector("#answer-4");
-var ans5Btn = document.querySelector("#answer-5");
+
+//Make the questions
+// Instructer said that we have to dio a minimum of 5 questions
 var questions = [
   {
     question: "Where do we put JavaScript code in an HTML document?",
@@ -81,11 +100,6 @@ var questions = [
       "How do you write an 'if' statement in JavaScript to execute code only if 'i' is NOT equal to 5?",
     answers: ["if(i<>5)", "if i=!5 then", "if(i!=5)", "if i <>5", "if i <~>5"],
     correctAnswer: 2,
-  },
-  {
-    question: "Which of the following is NOT a JavaScript data type?",
-    answers: ["Number", "Boolean", "String", "Function", "null"],
-    correctAnswer: 4,
   },
 ];
 
